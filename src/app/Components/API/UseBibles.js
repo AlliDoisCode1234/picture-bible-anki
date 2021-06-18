089bcf732d11a86c2dd2ce3b9984510588f981fa
Josiah <josiah.james.fowler@gmail.com>
2021-06-18 13:34:38 -0400

    api updated - now to itermate and map new data set

--- src/app/Components/API/UseBibles.js
+++ src/app/Components/API/UseBibles.js
@@ -5,21 +5,23 @@ const useBibles = () => {
   const [bibles, setBibles] = React.useState({});
 
   const getBibles = React.useCallback(() => {
-    fetch("https://api.scripture.api.bible/v1/bibles", {
+    fetch("https://ajith-holy-bible.p.rapidapi.com/GetBooks", {
       method: 'get',
-      headers: { 'accept': 'application/json', 'api-key': '2e5d71a91294792ed187ecf7c3645b31' }
+      headers: { 'x-rapidapi-key': '2cc83f283fmsh085ca9948622069p1f86ffjsn270a6e7aac00', 'x-rapidapi-host': 'ajith-holy-bible.p.rapidapi.com', 'useQueryString' : true }
     })
       .then((response) => response.json())
       .then((data) => {  
-          console.log(data)
-        const biblesHashMap = data.data.reduce((hashMap, item) => {
-          const leftChar = item.id;
-          const rightChar = item.name;
-          hashMap[rightChar] = leftChar;
-          return hashMap;
-        }, {});
-        setBibles(biblesHashMap)
-        console.log(biblesHashMap)
+        //   console.log(data)
+          console.log(data.The_New_Testament)
+          console.log(data.The_Old_Testament)
+        // const biblesHashMap = data.data.reduce((hashMap, item) => {
+        //   const leftChar = item.id;
+        //   const rightChar = item.name;
+        //   hashMap[rightChar] = leftChar;
+        //   return hashMap;
+        // }, {});
+        // setBibles(biblesHashMap)
+        // console.log(biblesHashMap)
       })
       .catch((err) => alert(err.message))
   }, []);
