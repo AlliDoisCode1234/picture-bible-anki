f75f8f195a01d324941a1a625e49ece3f5598df9
Josiah <josiah.james.fowler@gmail.com>
2021-06-18 03:01:23 -0400

    header styles updated

--- src/app/Components/Header.js
+++ src/app/Components/Header.js
@@ -1,13 +1,31 @@
 import React from 'react'
 import './Header.css'
 import { Link } from 'react-router-dom'
+import AddIcon from '@material-ui/icons/Add';
+import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
+import MessageIcon from '@material-ui/icons/Message';
 
 const Header = () => {
     return (
         <div className="header">
-            <Link to="/home">
+            {/* <Link to="/home">
                 <h1>Link to Home</h1>
-            </Link>
+            </Link> */}
+
+            <div className="header__logoContainer">
+            <Link to='/'>
+                  <img 
+                      src="https://i.imgur.com/JEjwXLv.png" 
+                      alt="app logo" 
+                      className="header__logoContainerImage" 
+                  />
+                </Link>
+            </div>
+            <div className="header__mainMenu">
+                <AddIcon />
+                <LibraryBooksIcon />
+                <MessageIcon />
+            </div>
         </div>
     )
 }
--- src/app/Components/UserProfile.js
+++ src/app/Components/UserProfile.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import './UserProfile.css'
+import Header from './Header'
 
 const UserProfile = () => {
     return (
         <div className="userProfile">
-            <h1>user profile</h1>
+
+            <Header />
+            
+            {/* Picture Bible Component (showcase userPictureBible with saved Verse Cards) */}
+            
+            {/* Contribution/Anki Study Heatmap */}
+
+            {/* User Wall Shows Infinite Scroll of user most recent cards created */}
+
+            {/* User Decks like sidebar */}
+
+            {/* Footer */}
+
         </div>
     )
 }
