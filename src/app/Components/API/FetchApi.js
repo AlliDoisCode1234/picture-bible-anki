aedcba0a6eebd45bfc6edda83804c3b37732f10f
Josiah <josiah.james.fowler@gmail.com>
2021-06-18 04:48:25 -0400

    pulled data from bible api and displayed it in console

--- src/app/Components/API/FetchApi.js
+++ src/app/Components/API/FetchApi.js
@@ -4,14 +4,13 @@ const FetchApi = () => {
 
     const apiGet = () => {
         fetch("https://api.scripture.api.bible/v1/bibles", {
-            "method": "'GET'",
-            "headers": {
-                'Api-Key': '2e5d71a91294792ed187ecf7c3645b31',
-                'Accept': 'application/json'
+            method: 'get',
+            headers: { 'accept': 'application/json', 'api-key': '2e5d71a91294792ed187ecf7c3645b31'
             }
         })
             .then((response) => response.json())
             .then((json) => console.log(json))
+            .catch((err) => alert(err.message))
     }
 
     return (
