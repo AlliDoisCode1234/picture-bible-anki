d9404f2731b80cafb3c96fe10bd105f1bbc77e98
Josiah <josiah.james.fowler@gmail.com>
2021-06-19 18:41:58 -0400

    built book component for user picture bible progress

--- src/app/Components/Book.js
+++ src/app/Components/Book.js
@@ -1,9 +1,34 @@
 import React from 'react'
-import '/Book.css'
+import './Book.css'
 
-const Book = ({ chapters, verses }) => {
+const Book = ({ bookName, chapters, verses }) => {
+    // const [chapter, setChapter] = useState();
+    // const [verse, setVerse] = useState();
+    // const [bookName, setBookName] = useState();
+
     return (
         <div className="book">
+            <h2>{bookName}</h2>
+            <form action="" className="book__form">
+                <label for={chapters}>Chapter:</label>
+                    <select name={bookName} id={bookName}>
+                        <option>1</option>
+                        {/* {
+                            chapters.map((item) => {
+                                return <option value={item}>{item}</option>
+                            })
+                        } */}
+                    </select>
+                <label for={chapters}>Verse:</label>
+                    <select name={chapters} id={chapters}>
+                        <option>2</option>
+                        {/* {
+                            verses.map((item) => {
+                                return <option value={item}>{item}</option>
+                            })
+                        } */}
+                    </select>
+            </form>
             
         </div>
     )
