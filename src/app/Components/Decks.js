7edf038ef0da1da6058b8acac91d975542d92143
Josiah <josiah.james.fowler@gmail.com>
2021-06-20 06:40:13 -0400

    deck styles added on user profile

--- src/app/Components/Decks.js
+++ src/app/Components/Decks.js
@@ -5,16 +5,16 @@ import './Decks.css'
 const Decks = () => {
     return (
         <div className="decks">
-            <h3>My Decks</h3>
-            <ul>
-                <li>Default</li>
-                <li>Grace</li>
-                <li>Peace</li>
-                <li>Love</li>
-                <li>Joy</li>
-                <li>Gentleness</li>
-                <li>Patience</li>
-                <li>Kindness</li>
+            <h3 className="decks__myDecks">My Decks</h3>
+            <ul className="decks__decksList">
+                <li className="decks__decksListItem">Default</li>
+                <li className="decks__decksListItem">Grace</li>
+                <li className="decks__decksListItem">Peace</li>
+                <li className="decks__decksListItem">Love</li>
+                <li className="decks__decksListItem">Joy</li>
+                <li className="decks__decksListItem">Gentleness</li>
+                <li className="decks__decksListItem">Patience</li>
+                <li className="decks__decksListItem">Kindness</li>
                 {/* Map through all of the user decks */}
             </ul>
         </div>
